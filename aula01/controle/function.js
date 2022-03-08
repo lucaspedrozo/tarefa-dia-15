f8875158e2775f3671267f23a7965873a043b7dc
lucaspedrozo <lucasdasilvapedrozo12121@gmail.com>
2022-03-08 10:35:14 -0300

    commit

--- aula01/controle/function.js
+++ aula01/controle/function.js
@@ -1,21 +1,33 @@
 $(document).ready(function(){
-    //monitorar o clique do botao 
-    $('.btn-send').click(function(e){
-            e.preventDefault()
+
+  // Monitorar o clique em cima do botão btn-send
+  $('.btn-send').click(function(e){
+      e.preventDefault()
+
+      // Coletar as informações digitadas/selecionadas no formulário
       let dados = $('#form').serialize()
+      $('#retorno').empty()
 
       $.ajax({
-        type: 'POST',
-        datatype:'JSON',
-        assync: true,
-        data: dados,
-        url: '../modelo/retorno.php',
-        success: function(){
+          type: 'POST',
+          dataType: 'JSON',
+          assync: true,
+          data: dados,
+          url: '../modelo/retorno.php',
+          success: function(dados){
+            $('#retorno').append(`
+            <div class="col-12 col-sm-8 col-md-6">
+            <div class="alert-primary">
+                <h1 class="text-white text-center">
+                    ${dados.mensagem}
+                </h1>
+                <img src="../../img/${dados.tipo}" class="img-fluid">
+            </div>
+        </div>
+              `)
 
-
-        }
-      }
-      )
-    })
+          }
+      })
+  })
 
 })
--- aula01/modelo/retorno.php
+++ aula01/modelo/retorno.php
@@ -1,38 +1,79 @@
 <?php
 
-$NOME = $_REQUEST['NOME'];
-$ANIMAL = $_REQUEST['ANIMAL'];
 
-if(empty($NOME)){
 
-    $dados = array ( 
-        "tipo" => 'error',
-        "mensagem" => 'Existem(m) campo(s) obrigatorio(s) nao  preechido(s).'
-    );
-}
+    $NOME = $_REQUEST['NOME'];
 
-else{
+    $ANIMAL = $_REQUEST['ANIMAL'];
 
-    switch($ANIMAL){
-        case '1' : $dados = array (
-            "tipo" => 'cachorro.jfif',
-            "mensagem" => 'Ola ' .$NOME. ', sabemos que seu animal preferido e o cachorro'
+
+
+    if(empty($NOME)){
+
+        
+
+        $dados = array(
+
+            "tipo" => 'error',
+
+            "mensagem" => 'Existem(m) campo(s) obrigatório(s) não preenchido(s).'
+
         );
-        break;
-        case '2' : $dados = array (
-            "tipo" => 'peixe.jfif',
-            "mensagem" => 'Ola ' .$NOME. ', sabemos que seu animal preferido e o peixe'
-        );
-        break;
+
     
-    case '3' : $dados = array(
-        "tipo" => 'gato.jfif',
-        "mensagem" => 'Ola ' .$NOME. ', sabemos que seu animal preferido e gato'
-    );
-}
 
-echo json_encode($dados);
+    } else {
 
 
 
-}
+        switch($ANIMAL){
+
+            case '1' : $dados = array(
+
+                "tipo" => 'cachorro.jpg',
+
+                "mensagem" => 'Olá '.$NOME.', sabemos que seu animal preferido é o cachorro'
+
+            );
+
+            break;
+
+            case '2' : $dados = array(
+
+                "tipo" => 'gato.jpg',
+
+                "mensagem" => 'Olá '.$NOME.', sabemos que seu animal preferido é gato'
+
+            );
+
+            break;
+
+            case '3' : $dados = array(
+
+                "tipo" => 'peixe.jpg',
+
+                "mensagem" => 'Olá '.$NOME.', sabemos que seu animal preferido é o peixe'
+
+            );
+
+            break;
+            case '4' : $dados = array(
+
+                "tipo" => 'porco.jpg',
+
+                "mensagem" => 'Olá '.$NOME.', sabemos que seu animal preferido é o porco'
+
+            );
+
+            break;
+
+
+        }
+
+
+
+    }
+
+
+
+    echo json_encode($dados);
